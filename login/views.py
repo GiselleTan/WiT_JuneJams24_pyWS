c902f819b6e74c99d7f37aaa021ac4d5e4a8ea48
khuiqian21 <122453165+khuiqian21@users.noreply.github.com>
2024-01-12 19:16:32 +0900

    sign up forms

--- login/views.py
+++ login/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import LoginForm
+
+
 
 def login_view(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = AuthenticationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
+            user = form.get_user()
             password = form.cleaned_data['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
