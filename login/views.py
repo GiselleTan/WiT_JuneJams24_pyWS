8891b10bd85429a481b88d6781eb3f838246d02e
CelestineTan03 <e0968156@u.nus.edu>
2024-01-15 13:32:30 +0900

    Update login authentication

--- login/views.py
+++ login/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import LoginForm, SignupForm
 
-
-
 def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
+            username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')  # Change 'home' to the desired URL
+                return redirect('topic_index')
     else:
         form = LoginForm()
 
